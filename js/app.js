644db74db956d2f23f1f5317143c73fc34c9cf2e
Kamal <theoriginalkamal@gmail.com>
2020-05-30 15:06:51 +0530

    added auto hide feature for nav bar

--- js/app.js
+++ js/app.js
@@ -1,70 +1,178 @@
 /**
- * Global Variables
- * 
-*/
-
-let navBar = document.querySelector("#navbar__list");
-let sections = document.querySelectorAll(".navbar-section");
-let length = sections.length;
-let sectionPosition = [];
-let oldPosition = 0;
-let currentPosition = 0;
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
 
 /**
- * Start Helper Functions
- * 
-*/
+ * Define Global Variables
+ *
+ */
 
-function jumpToSection(sectionID) {
-	window.scrollTo(0, sectionID);
+// sections is an HTMLCollection live and ordered, which does not include spaces.
+const sections = document.getElementsByTagName("section");
+const nav = document.querySelector("#navbar__list");
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+function insertNavLink(navLinkName, sectionId) {
+	// create a link
+	const htmlTextToAdd = `<li><a href="#${sectionId}" class="menu__link">${navLinkName}</a></li>`;
+	// add it to the nav
+	nav.insertAdjacentHTML("beforeend", htmlTextToAdd);
+}
+
+function isTopSectionInViewport(el) {
+	// get the coordinates of the element box from the viewport
+	const rect = el.getBoundingClientRect();
+	// return true if the element top is near the top of the viewport, false otherwise
+	return (
+		rect.top >= 0 &&
+		// 0.4 works well across devices without overlapping sections
+		rect.top <=
+		0.4 * (window.innerHeight || document.documentElement.clientHeight)
+	);
+}
+
+// simple and perfect helper function taken from https://muffinman.io/javascript-get-element-offset/
+function getElementOffset(el) {
+	// get the coordinates of the element box from the viewport
+	const rect = el.getBoundingClientRect();
+	// return element absolute top/left position as an object (element scroll in viewport + document scroll)
+	return {
+		top: rect.top + window.pageYOffset,
+		left: rect.left + window.pageXOffset
+	};
 }
 
 /**
+ * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
-// Bulding the navigation bar
-sections.forEach((element, ind) => {
-	let sectionName = element.getAttribute("data-nav");
-	let navSection = document.createElement("li");
-	let toOfSection = element.offsetTop;
-	navSection.setAttribute("class", "menu__link" + ind);
-	navSection.innerHTML = `<a onClick= "jumpToSection(${toOfSection})">${sectionName}</a>`;
-	navBar.appendChild(navSection);
-});
-
-// Check the active section and scrolling
-document.addEventListener("scroll", () => {
-	// Check if the user is scrolling or not
-	if (currentPosition != oldPosition) {
-		document.querySelector(".page__header").style.visibility = "visible";
+// build the nav
+function buildNav(sections) {
+	for (const section of sections) {
+		let navLinkName = section.getAttribute("data-nav");
+		let sectionId = section.getAttribute("id");
+		insertNavLink(navLinkName, sectionId);
 	}
-	currentPosition = this.scrollY;
-	// Section Positions	
-	sectionPosition = [];
-	sections.forEach(element => {
-		let toOfSection = element.getBoundingClientRect().top;
-		sectionPosition.push(toOfSection);
+}
+
+// Add class 'active' to section when near top of viewport
+function setSectionIntoViewActive(sections) {
+	for (const section of sections) {
+		// detect the navigation link that matches the current section
+		const activeLink = document.querySelector(
+			`a[href="#${section.getAttribute("id")}"]`
+		);
+		// checks if the current section is near top of viewport
+		if (isTopSectionInViewport(section)) {
+			// if it is, highlight menu link and section with active styles
+			section.classList.add("active");
+			activeLink.classList.add("menu__link--active");
+		} else {
+			// if it is not, remove active styles
+			section.classList.remove("active");
+			activeLink.classList.remove("menu__link--active");
+		}
+	}
+}
+
+// Scroll to section anchor ID using smooth scrollTO event
+function smoothScroll(el) {
+	window.scrollTo({
+		//scroll to element and account for sticky header offset
+		top: getElementOffset(el).top - nav.offsetHeight,
+		left: getElementOffset(el).left,
+		behavior: "smooth"
 	});
-	// Adding and removing active sections
-	addIndex = sectionPosition.findIndex(element => element > 0);
-	for (let i = 0; i < length; i++) {
-		if (addIndex === i) {
-			document.querySelector(".menu__link" + addIndex).classList.add("active");
-		}
-		else {
-			document.querySelector(".menu__link" + i).classList.remove("active");
-		}
+}
+
+// create a "Go to Top" hidden floating button
+function createBtnUp() {
+	const htmlTextToAdd = `<a href="#" class="bottom__link hide">To top</a>`;
+	document.body.insertAdjacentHTML("afterbegin", htmlTextToAdd);
+}
+
+function showBtnUp() {
+	// create and add the buttom to the DOM as hidden element
+	const btn = document.querySelector(".bottom__link");
+	// remove the button if absolute scrolling y is lower than browser window viewport
+	if (window.pageYOffset <= 0.6 * window.innerHeight) {
+		btn.classList.add("hide");
+	} else {
+		btn.classList.remove("hide");
+		// add event handler only if button is visible
+		btn.addEventListener("click", function (e) {
+			e.preventDefault();
+			// smooth scroll to top of page
+			window.scrollTo({
+				top: 0,
+				left: 0,
+				behavior: "smooth"
+			});
+		});
 	}
+}
+
+// show navigation bar after delay is elapsed, when used with scroll events,
+// timer gets reset and until the user stops scrolling
+function hideNav(delay) {
+	var timer;
+	timer && clearTimeout(timer);
+	nav.classList.add("hide");
+	timer = setTimeout(function () {
+		nav.classList.remove("hide");
+	}, delay);
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
+	// Build menu and hidden Button up on page load
+	buildNav(sections);
+	createBtnUp();
+
+	// Scroll to section on link click
+	// using event delegation
+	nav.addEventListener("click", function (e) {
+		if (e.target.nodeName === "A") {
+			e.preventDefault();
+			// getting the clicked link
+			const activeSection = document.querySelector(
+				`section[id = ${e.target.getAttribute("href").slice(1)}]`
+			);
+			// smooth scrolling to the clicked link
+			smoothScroll(activeSection);
+		}
+	});
+
+	// Set sections as active, hide nav when scrolling and show button up after 1st screen
+	setTimeout(
+		window.addEventListener("scroll", function () {
+			setSectionIntoViewActive(sections);
+			hideNav(1000);
+			showBtnUp();
+		}),
+		// this is a delay to reduce the frequency of scrolling events fired
+		2000
+	);
 });
-// Hide the menu if the user is no longer scrolling
-setInterval(function () {
-	if (currentPosition === oldPosition) {
-		document.querySelector(".page__header").style.visibility = "hidden";
-	}
-	else {
-		document.querySelector(".page__header").style.visibility = "visible";
-	}
-	oldPosition = currentPosition;
-}, 20000);
